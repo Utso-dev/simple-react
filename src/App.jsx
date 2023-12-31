8a55044a9159d2bf86930eef6f906aa6dea42e1e
Utso sarkar <utsosarkar7870@gmail.com>
2023-12-31 07:44:20 +0600

    this commite siple react app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-3xl text-center  text-red-500 underline font-bold'>hello word!</h1>
+       
+    </>
+  )
+}
+
+export default App
